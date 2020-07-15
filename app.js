deb5a5ffe01e85a1d942a6c59d424b773fbd2be1
mizzerx <vuhunglcute@gmail.com>
2020-07-15 17:00:20 +0700

    Add new function write note

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: __dirname});
@@ -28,5 +29,25 @@ app.post('/doRegister', function (req, res) {
 
   res.send(`Hello ${name} your pass is ${pass}`);
 });
+
+app.get('/note', function(req, res) {
+  res.sendFile('note.html', {root: __dirname});
+});
+
+app.post('/takeNote', function(req, res) {
+  const note = req.body.note;
+  const writeNote = Date() + "\n" + note;
+  fs.appendFile('note.txt', writeNote, function(err, data) {
+    if (err) res.send('Write fail');
+    else res.send('Wite success');
+  })
+});
+
+app.get('/readNote', function(req, res) {
+  fs.readFile('note.txt', function(err, data) {
+    if (err) res.send('Read note error');
+    else res.send(`${data}`);
+  })
+})
 
 app.listen(5050);
